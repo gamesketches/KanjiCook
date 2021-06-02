3f6ef9f2f9b80fd062d1464752c1638fcbe05943
gamesketches <sam.von.ehren@gmail.com>
2021-06-02 15:58:01 -0400

    Parsing kanji json, will need to make an adjustment to the structure from the output script

---
+++ Assets/ContentManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContentManager : MonoBehaviour
+{
+	public TextAsset kanjiFile;
+	KanjiInfoFile myKanji;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myKanji = JsonUtility.FromJson<KanjiInfoFile>(kanjiFile.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class KanjiInfoFile {
+	public KanjiInfo[] kanjiInfos;
+}
+
+[Serializable]
+public class KanjiInfo {
+	public string[] meanings;
+	public string kanji;
+	public string[] radicals;
+}
