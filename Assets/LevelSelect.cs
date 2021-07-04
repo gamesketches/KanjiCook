ff4acfccb41a692fc84a03f8a37941fe3565be8f
gamesketches <sam.von.ehren@gmail.com>
2021-07-04 19:05:00 -0400

    Adding in level select code some basic animations for the chef, replacing some of the art

---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour
+{
+	public GameObject levelButtonPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+		int levelCount = 0;
+     	foreach(TextAsset asset in Resources.LoadAll<TextAsset>("")) {
+			GameObject levelButton = Instantiate(levelButtonPrefab);
+			levelButton.transform.parent = transform;
+			//levelButton.GetComponent<Image>().onClick.AddListener(() => LevelLoadCallback(levelCount));
+			levelButton.GetComponent<LevelSelectButton>().Initialize(levelCount, levelCount.ToString());
+			levelCount++;
+		} 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void LevelLoadCallback(int level) {
+		Debug.Log("loading level " + level.ToString());
+	}
+}
---
+++ Assets/LevelSelectButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LevelSelectButton : MonoBehaviour, IPointerClickHandler
+{
+	int levelId;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void Initialize(int id, string levelName) {
+		levelId = id;
+		GetComponentInChildren<Text>().text = levelName;
+	}
+
+	public void OnPointerClick(PointerEventData pointerEventData) {
+		Debug.Log("clicked " + levelId.ToString());
+	}
+}
