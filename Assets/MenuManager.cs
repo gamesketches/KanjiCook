88ce4da2be6d08b1e51dbd0a809d6a4e17133e30
gamesketches <sam.von.ehren@gmail.com>
2021-07-13 18:50:00 -0400

    Changing requests to match art, changing plates and result pot to match art, adding in title screen that transitions to main menu

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+	public GameObject levelSelect;
+	public GameObject titleScreen;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        titleScreen.SetActive(true);
+		levelSelect.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void OpenMainMenu() {
+		DismissTitleScreen();
+	}
+
+	void DismissTitleScreen() {
+		titleScreen.SetActive(false);
+	}
+}
