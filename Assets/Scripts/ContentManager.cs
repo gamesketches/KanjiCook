a9831c139b9b0c8869d94cc32e4cd1bc8f6f4468
gamesketches <sam.von.ehren@gmail.com>
2021-08-01 18:17:45 -0400

    Fixed issue with certain characters not displaying, had to bring in a new font

--- Assets/Scripts/ContentManager.cs
+++ Assets/Scripts/ContentManager.cs
@@ -48,6 +48,12 @@ public class ContentManager : MonoBehaviour
 		foreach(KanjiInfo kanji in levelKanji.kanjiInfos) {
 			Debug.Log("Adding kanji " + kanji.kanji);
 			int randomMeaning = Random.Range(0, kanji.meanings.Length);
+			for(int i = 0; i < tempContent.Count; i++) {
+				if(tempContent[i].target == kanji.meanings[randomMeaning]) {
+					i = -1;
+					randomMeaning = Random.Range(0, kanji.meanings.Length);
+				}
+			}
 			tempContent.Add(new LanguagePair(kanji.meanings[randomMeaning], kanji.kanji, kanji.radicals, kanji.kunyomi, kanji.onyomi));
 		}
 		return tempContent.ToArray();
