ca9d921aa698cca5ed7c4064fb7922d29f8168aa
gamesketches <sam.von.ehren@gmail.com>
2021-10-05 21:18:44 -0400

    Adding in new entree design, updating entree script to handle it

--- Assets/Scripts/EntreeBehavior.cs
+++ Assets/Scripts/EntreeBehavior.cs
@@ -18,8 +18,8 @@ public class EntreeBehavior : MonoBehaviour
 
 	public void Initialize(EntreeData entreeData) {
 		character.text = entreeData.literal;
-		character.fontSize = literalFontSize;
-		string formulaString = "= " + entreeData.components[0];
+		//character.fontSize = literalFontSize;
+		string formulaString = entreeData.components[0];
 		for(int i = 1; i < entreeData.components.Length; i++) {
 				formulaString += " + " + entreeData.components[i];
 		}
@@ -28,20 +28,21 @@ public class EntreeBehavior : MonoBehaviour
 		string reading = GenerateReadingString(entreeData.kunyomi, entreeData.onyomi);
 		string definitionList = GenerateDefinitionString(entreeData.meanings);
 		definitionString += reading + definitionList;
-		definition.text = definitionString;
+		definition.text = definitionList;
 	}
 
 	string GenerateReadingString(string[] kunyomi, string[] onyomi) {
-		string hiraganaReading = "むら";
+		string hiraganaReading = "";
 		if(kunyomi.Length > 0) hiraganaReading = kunyomi[0];
 		else if(onyomi.Length > 0) hiraganaReading = onyomi[0];
+		//return " (" + hiraganaReading + "|" + GetRomanji(hiraganaReading) + ") ";
 		return " (" + hiraganaReading + "|" + GetRomanji(hiraganaReading) + ") ";
 	}
 
 	string GenerateDefinitionString(string[] meanings) {
-		string returnString = "\"" + meanings[0] + "\"";
+		string returnString = meanings[0];
 		for(int i = 1; i < meanings.Length; i++) {
-			returnString += ", \"" + meanings[i] + "\"";
+			returnString += ", " + meanings[i];
 		}
 		return returnString;
 	}
