9837e6043f5f46ffc0cb9972a546815232d61b90
gamesketches <sam.von.ehren@gmail.com>
2022-01-01 20:32:47 -0500

    Adding in basic tutorial behavior

---
+++ Assets/Scripts/TutorialImageController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TutorialImageController : MonoBehaviour, IPointerClickHandler
+{
+	public Sprite[] tutorialImages;
+	Image tutorialImage;
+	int numImages;
+	int imageIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numImages = tutorialImages.Length;
+		imageIndex = 0;
+		tutorialImage = GetComponent<Image>();
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData) {
+		imageIndex++;
+		if(imageIndex < tutorialImages.Length) {
+			tutorialImage.sprite = tutorialImages[imageIndex];
+		} else {
+			MenuManager.instance.BackButtonPressed();
+		}
+	}
+}
